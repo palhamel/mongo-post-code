78f11dd3096f118e68d43c71616eddf185c4fe2d
palhamel <pal.hamel@gmail.com>
2020-05-16 09:40:49 +0200

    Set up mongoose model

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+
+// Database setup:
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/mongo-post-codealong";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Mongoose model setup:
+
+const Task = mongoose.model("Task", {
+  text: {
+    type: String,
+    required: true,
+    minlenght: 5
+  },
+  complete: {
+    type: Boolean,
+    default: false  
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+
+
+
+
+
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -15,7 +50,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello funny world')
 })
 
 // Start the server
